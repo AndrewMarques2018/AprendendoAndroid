a306a512ad90b6fdaec95774a0746a683d3cff4f
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-09-11 10:50:17 -0300

    appCaraouCoroa

--- appCaraOuCoroa/app/src/main/java/com/andrewmarques/android/appcaraoucoroa/MainActivity.java
+++ appCaraOuCoroa/app/src/main/java/com/andrewmarques/android/appcaraoucoroa/MainActivity.java
@@ -2,13 +2,32 @@ package com.andrewmarques.android.appcaraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btJogar = findViewById(R.id.btJogar);
+
+        btJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void trocarTela (View view){
+        setContentView(R.layout.activity_resultado);
     }
 }
