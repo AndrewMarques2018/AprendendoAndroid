81f2e535049d12543b65066d5d545f18f406840e
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-09-18 17:57:26 -0300

    App Lista de Tarefas
    Tentativa numero 3

---
+++ AppListaDeTarefas3/app/src/main/java/com/andrewmarques/android/applistadetarefas3/AdicionarTarefaAcitivity.java
@@ -0,0 +1,13 @@
+package com.andrewmarques.android.applistadetarefas3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionarTarefaAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+    }
+}
