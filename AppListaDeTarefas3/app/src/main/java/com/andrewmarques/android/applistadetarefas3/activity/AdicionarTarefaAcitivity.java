8127104dac6fdf76260280fac4e33ff3d4da25ab
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-09-18 22:59:40 -0300

    App Lista de Tarefas
    Criando icon salvar no Menu em AdicionarTarefaActivity

--- AppListaDeTarefas3/app/src/main/java/com/andrewmarques/android/applistadetarefas3/activity/AdicionarTarefaAcitivity.java
+++ AppListaDeTarefas3/app/src/main/java/com/andrewmarques/android/applistadetarefas3/activity/AdicionarTarefaAcitivity.java
@@ -1,7 +1,10 @@
 package com.andrewmarques.android.applistadetarefas3.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.andrewmarques.android.applistadetarefas3.R;
 
@@ -11,5 +14,21 @@ public class AdicionarTarefaAcitivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_salvar:
+                // Executar uma acao para o item salvar
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
